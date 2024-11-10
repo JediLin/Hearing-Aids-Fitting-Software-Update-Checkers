dacdc531479cf6d7924efd0d589fc52339139a60
Jedi Lin <JediLin@Gmail.com>
2024-11-10 10:32:03 +0800

    Update start-Linux-macOS.sh
    atuomatically install Python packages update

--- start-Linux-macOS.sh
+++ start-Linux-macOS.sh
@@ -5,8 +5,8 @@ cd -- "$(dirname "$0")"
 
 clear
 echo "Installing requirements..."
-pip install -r ./requirements.txt
-pip install -r ./requirements_uncertain.txt
+pip install --upgrade -r ./requirements.txt
+pip install --upgrade -r ./requirements_uncertain.txt
 
 clear
 python "./Checker Menu.py"
