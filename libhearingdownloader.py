0d9906a891602e2576978282e4a81ab47c49202b
Jedi Lin <JediLin@Gmail.com>
2025-01-28 10:51:27 +0800

    ready for v2025.01.28

--- libhearingdownloader.py
+++ libhearingdownloader.py
@@ -27,8 +27,8 @@ import os
 # libhearingdownloader - A useful library for the downloader scripts
 ###
 
-downloaderVersion = "Pre-release"
-#downloaderVersion = "v2024.12.22"
+#downloaderVersion = "Pre-release"
+downloaderVersion = "v2025.01.28"
 updaterRetries = 3
 verboseDebug = False
 
