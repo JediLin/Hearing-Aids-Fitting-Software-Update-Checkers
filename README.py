39367549175eb15ba95ab7e6dfe9022c47832266
Jedi Lin <JediLin@Gmail.com>
2025-05-10 09:02:31 +0800

    read-only

--- README.py
+++ README.py
@@ -3,7 +3,7 @@ from rich.markdown import Markdown
 from rich.panel import Panel
 
 console = Console(width=80)
-with open("CHANGES.md", "r+", encoding="utf-8") as changeFile:
+with open("CHANGES.md", "r", encoding="utf-8") as changeFile:
     changeLog = changeFile.read()
     console.print(Panel(Markdown(changeLog), padding=2))
     print("  Please scroll back manually to read the whole changelog.")
