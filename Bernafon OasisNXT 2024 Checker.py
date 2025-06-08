4d3441eca52f6a4879f16736cd24364fbff56f90
Jedi Lin <JediLin@Gmail.com>
2025-06-08 21:58:47 +0800

    configuration for Bernafon

--- Bernafon OasisNXT 2024 Checker.py
+++ Bernafon OasisNXT 2024 Checker.py
@@ -3,6 +3,7 @@
 #                   Copyright Bluebotlabz                   #
 #                                                           #
 #############################################################
+import configparser
 import html
 import requests
 from pathlib import Path
@@ -47,8 +48,12 @@ disclaimer = [
 if not turboFile.is_file():
     libhearingdownloader.printDisclaimer(disclaimer)
 
+# Read configuration file for toggles with default True
+config = configparser.ConfigParser()
+config.read('config.ini')
+fallbackMarket = config.get('Bernafon', 'Market', fallback='Default')
+
 # Read target market from GitHub or local configuration
-fallbackMarket = "Default"
 localMarketPath = Path("Bernafon.market")
 onlineMarketPath = "https://github.com/JediLin/Hearing-Aids-Fitting-Software-Update-Checkers/raw/refs/heads/main/Bernafon.market"
 defaultMarketSrc = ""
@@ -108,15 +113,17 @@ while updaterRetries > 0:
         # This is required to get update for newer (2024+) versions.
         # Software names set by b:Name
         # Version numbers set by b:Major . b:Minor . b:Build . b:Revision
-        osVer = "Microsoft Windows NT 10.0.22621.0"
-        baseVerMajor = "20"
-        baseVerMinor = "22"
-        baseVerBuild = "95"
-        baseVerRev = "0"
-        updrVerMajor = "27"
-        updrVerMinor = "3"
-        updrVerBuild = "26"
-        updrVerRev = "0"
+        osVer = config.get('Bernafon', 'OS', fallback='Microsoft Windows NT 10.0.22621.0')
+        baseVer = config.get('Bernafon', 'Version', fallback='20.22.95.0')
+        updrVer = config.get('Bernafon', 'Updater', fallback='27.3.26.0')
+        baseVerMajor = baseVer.split('.')[0]
+        baseVerMinor = baseVer.split('.')[1]
+        baseVerBuild = baseVer.split('.')[2]
+        baseVerRev = baseVer.split('.')[3]
+        updrVerMajor = updrVer.split('.')[0]
+        updrVerMinor = updrVer.split('.')[1]
+        updrVerBuild = updrVer.split('.')[2]
+        updrVerRev = updrVer.split('.')[3]
         rawXmlData = requests.post("https://updater.bernafon.com/UpdateWebService.svc", headers=headers, data='<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://www.w3.org/2005/08/addressing"><s:Header><a:Action s:mustUnderstand="1">http://tempuri.org/IUpdateWebService/CheckForUpdate</a:Action><a:MessageID>urn:uuid:00000000-0000-0000-0000-000000000000</a:MessageID><a:ReplyTo><a:Address>http://www.w3.org/2005/08/addressing/anonymous</a:Address></a:ReplyTo><a:To s:mustUnderstand="1">https://updater.bernafon.com/UpdateWebService.svc</a:To></s:Header><s:Body><CheckForUpdate xmlns="http://tempuri.org/"><request xmlns:b="http://schemas.datacontract.org/2004/07/Wdh.Genesis.SoftwareUpdater.Common" xmlns:i="http://www.w3.org/2001/XMLSchema-instance"><b:ClientId>00000000-0000-0000-0000-000000000000</b:ClientId><b:Languages xmlns:c="http://schemas.microsoft.com/2003/10/Serialization/Arrays"/><b:Locale>' + targetMarket + '</b:Locale><b:Manufacturer>Bernafon</b:Manufacturer><b:OEM>Bernafon</b:OEM><b:OS>' + osVer + '</b:OS><b:RequestVersion>1</b:RequestVersion><b:Software><b:InstalledSoftware><b:Build>' + updrVerBuild + '</b:Build><b:Major>' + updrVerMajor + '</b:Major><b:Minor>' + updrVerMinor + '</b:Minor><b:Name>BernafonUpdater</b:Name><b:Revision>' + updrVerRev + '</b:Revision></b:InstalledSoftware><b:InstalledSoftware><b:Build>' + baseVerBuild + '</b:Build><b:Major>' + baseVerMajor + '</b:Major><b:Minor>' + baseVerMinor + '</b:Minor><b:Name>Oasis2</b:Name><b:Revision>' + baseVerRev + '</b:Revision></b:InstalledSoftware></b:Software></request></CheckForUpdate></s:Body></s:Envelope>')
         data = xml.fromstring(html.unescape(rawXmlData.text))
         break
