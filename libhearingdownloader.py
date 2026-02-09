3aa9dfa36e602f3e7507ac526d006edad5ddb327
Jedi Lin <JediLin@Gmail.com>
2026-02-09 21:00:43 +0800

    ready for v2026.02.09 Release

--- libhearingdownloader.py
+++ libhearingdownloader.py
@@ -28,8 +28,8 @@ import os
 # libhearingdownloader - A useful library for the downloader scripts
 ###
 
-downloaderVersion = "Pre-release"
-#downloaderVersion = "v2026.01.30"
+#downloaderVersion = "Pre-release"
+downloaderVersion = "v2026.02.09"
 updaterRetries = 3
 # Read configuration file for toggles with default True
 config = configparser.ConfigParser()
